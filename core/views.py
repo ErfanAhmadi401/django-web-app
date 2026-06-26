0303b9655a0777b7074d72f05a34040f7680e63f
Erfan <erfanahmadi401401@gmail.com>
2026-06-26 22:54:23 +0330

    refactor: move URL routing to app-level configuration
    - Create app-level URL configuration
    - Move route definitions to core/urls.py
    - Connect app routes using include()
    - Organize project URL routing following Django conventions

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("Hello World!")
+
+
+def contact(request):
+    return HttpResponse("Contact us")
+
+
+def about(request):
+    return HttpResponse("About Us")
