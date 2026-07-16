928e71a5333c4041c541b8510a7876cd02c9efa1
Erfan <erfanahmadi401401@gmail.com>
2026-07-16 04:59:11 +0330

    refactor(blog): replace custom snippet with built-in truncatechars filter
    Use Django's built-in template filter instead of custom model method.
    
    - Remove snippets method from Post model
    - Replace {{ post.snippets }} with {{ post.content|truncatechars:200 }} in template
    - Clean up commented code
    - Minor cleanup in requirements.txt

--- blog/models.py
+++ blog/models.py
@@ -45,5 +45,5 @@ class Post(models.Model):
     def __str__(self):
         return "{} - {}".format(self.title, self.id)
     
-    def snippets(self):
-        return self.content[:100] + " ..."
+    # def snippets(self):
+    #     return self.content[:100] + " ..."
